7bb5db13d327b7a2aa3a0898bd0b8b271bbfe6c6
Eric Chiu <c3366301@gmail.com>
2016-06-23 00:02:25 +0800

    "User can post topic"

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,29 @@
 class TopicsController < ApplicationController
 
+  before_action :authenticate_user!, :only => [:new, :create, :destroy]
+
   def index
     @topics = Topic.page(params[:page])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to topics_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title, :content)
   end
 
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,12 @@
 class Topic < ActiveRecord::Base
 
-  belongs_to :user
+  validates_presence_of :title, :content
 
+  belongs_to :user
   has_many :comments
+
+  def author_name
+    user.try(:display_name) || "Nobody"
+  end
 
 end
